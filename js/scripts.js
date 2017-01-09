4f246a02ff41570aecf52fb216afe73d574191fb
Benjamin T. Seaver and Jake Campa <benjamin+jake@>
2017-01-09 12:05:22 -0800

    add gallons - liters conversion

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@ var saySomething = function(sayWhat) {
 var getString = function(myPrompt) {
 	return prompt(myPrompt);
 };
+var convertGallonsLiters = function (gallonsOrLiters, gallonsToLiters) { //if true gallons to liters else liters to gallons
+	if (gallonsToLiters) {
+		var result = gallonsOrLiters * 3.785411784;
+	} else {
+		var result = gallonsOrLiters * (1/3.785411784);
+	};
+	return result;
+}
+
+// Test Section:
+saySomething("Convert 5 gallons to liters: " + convertGallonsLiters(5, true));
+saySomething("Convert 5 liters to gallons: " + convertGallonsLiters(5, false));
